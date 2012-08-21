ab1d5a37206b23988ab76f7129aaa5de1dab8b03
Michele Ceriotti <michele.ceriotti@gmail.com>
2012-08-21 08:56:24 +0100

    Added a nifty little prog to do FFT from the CLI

---
+++ src/tools/fourier.cpp
@@ -0,0 +1,68 @@
+/* This does not really use much of the toolbox infrastructure, 
+   but is a handy interface to do Fourier transform from the CLI
+   such as in BASH scripts and the like.
+   */
+   
+#include "clparser.hpp"
+#include "fftw3.h"
+using namespace toolbox;
+
+void banner()
+{
+   std::cerr<< " USAGE: fourier [options] < INPUT [ > OUTPUT ]                                  \n"
+            << " reads a time series from stdin and computes its Fourier transform by FFT.      \n"
+            << " Input should be just the time series, without headers or time code.            \n"
+            << " Data can be interpreted in different ways, depending to the options given:     \n"
+            << " @@@@@@@@@@@@@@@@@@@@@ DEFINE DIFFERENT BEHAVIOURS @@@@@@@@@@@@@@@@@@@@@@@      \n"
+            << " -dt [dt]    sets the time interval between input samples. {def: 1.0}           \n"
+            << " -pad [npad] appends npad zeroes before doing the FT. increases the resolution. \n"
+            << " -win [wnd]  applies a windowing function to the data before FT.                \n"
+            << "             possible values: triangle | cosine | hanning                       \n"
+            << "                                                                                \n";
+}
+
+int main(int argc, char ** argv)
+{
+   CLParser clp(argc, argv);
+   
+   double dt=1.0; 
+   unsigned long npad=0;
+   std::string wnd;
+   
+   bool fok=clp.getoption(dt, "dt", 1.0) &
+            clp.getoption(npad, "pad", (unsigned long) 0) &   
+            clp.getoption(wnd, "win", std::string(""));
+   
+   if (!fok) { banner(); return -1; }
+   
+   std::vector<double> data(0); double y;
+   // fills up the time series array from stdin 
+   while (std::cin.good()) { std::cin >> y; data.push_back(y); }
+   
+   // prepares for fft: transfers into valarray, pads, boxes etc
+   unsigned long ndata=data.size(), nfft=ndata+npad;
+   std::valarray<double> vvt(nfft), vvw(nfft);
+   vvt=0.0;  for (unsigned long i=0 ; i<ndata; ++i) vvt[i]=data[i];
+
+   if (wnd=="triangle")
+      for (unsigned long i=0 ; i<ndata; ++i) vvt[i]*=(1.-double(i)/double(ndata-1));
+   else if (wnd=="cosine")
+      for (unsigned long i=0 ; i<ndata; ++i) vvt[i]*=cos(0.5*constant::pi*double(i)/double(ndata-1));   
+   else if (wnd=="hanning")
+      for (unsigned long i=0 ; i<ndata; ++i) vvt[i]*=0.5*(1.0+cos(constant::pi*double(i)/double(ndata-1)));   
+   else if (wnd!="") ERROR("Window function "<< wnd << " mispelled or not implemented.");
+   // FFTW calls
+   fftw_plan r2rplan=fftw_plan_r2r_1d(nfft, &vvt[0], &vvw[0], FFTW_REDFT00, FFTW_ESTIMATE);
+   fftw_execute(r2rplan);        
+   
+   // fixes the normalization
+   vvw*=dt/(2.*constant::pi);
+   double dw=constant::pi/(dt*(nfft-1));
+
+   // very terse output
+   std::cout.precision(8); std::cout.width(15); std::cout.setf(std::ios::scientific); 
+   for (unsigned long i=0; i<nfft; ++i) std::cout<<i*dw<<"  "<<vvw[i]<<std::endl;
+   
+  return 0;               
+}
+
